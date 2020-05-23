76e64a6738400bbf29d1b7e9d07169ec147cf281
Dante Tobar <dante.m.tobar@gmail.com>
2020-05-23 15:25:23 -0700

    Added name field and password functions

--- server/db/models/User.js
+++ server/db/models/User.js
@@ -1,7 +1,13 @@
 const Sequelize = require('sequelize');
+const crypto = require('crypto');
+
 const db = require('../db');
 
 const User = db.define('user', {
+  name: {
+    type: Sequelize.STRING,
+    allowNull: false,
+  },
   email: {
     type: Sequelize.STRING,
     unique: true,
@@ -15,6 +21,9 @@ const User = db.define('user', {
     type: Sequelize.STRING,
     allowNull: false,
   },
+  salt: {
+    type: Sequelize.STRING,
+  },
 
   balance: {
     type: Sequelize.INTEGER,
@@ -22,12 +31,47 @@ const User = db.define('user', {
   },
 });
 
+/**
+ * instanceMethods
+ */
+User.prototype.correctPassword = function(candidatePwd) {
+  return User.encryptPassword(candidatePwd, this.salt) === this.password;
+};
+
 // Removes password when sent to client
 User.prototype.toJSON = function() {
-  var values = Object.assign({}, this.get());
+  const values = Object.assign({}, this.get());
 
   delete values.password;
+  delete values.salt;
   return values;
 };
+
+User.generateSalt = function() {
+  return crypto.randomBytes(16).toString('base64');
+};
+
+User.encryptPassword = function(plainText, salt) {
+  return crypto
+    .createHash('RSA-SHA256')
+    .update(plainText)
+    .update(salt)
+    .digest('hex');
+};
+
+const setSaltAndPassword = user => {
+  if (user.changed('password')) {
+    user.salt = User.generateSalt();
+    user.password = User.encryptPassword(user.password, user.salt);
+  }
+};
+
+User.beforeCreate(user => {
+  setSaltAndPassword(user);
+});
+
+User.beforeUpdate(user => {
+  setSaltAndPassword(user);
+});
 
 module.exports = User;
