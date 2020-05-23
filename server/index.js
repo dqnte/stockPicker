7014810ba801316dacf62ff661ea0245978cc0a5
Dante Tobar <dante.m.tobar@gmail.com>
2020-05-23 10:18:31 -0700

    Session middleware added

--- server/index.js
+++ server/index.js
@@ -3,11 +3,42 @@ const path = require('path');
 const express = require('express');
 const app = express();
 
+// Secrets for development
+if (process.env.NODE_ENV !== 'production') require('../secrets');
+
 // Default port is 3000
 const PORT = process.env.PORT || 3000;
 
 // Db Instance
 const { db } = require('./db');
+
+// Passport Registration
+const passport = require('passport');
+passport.serializeUser((user, done) => done(null, user.id));
+
+passport.deserializeUser(async (id, done) => {
+  try {
+    const user = await db.models.user.findByPk(id);
+    done(null, user);
+  } catch (err) {
+    done(err);
+  }
+});
+
+// Session Middleware
+const session = require('express-session');
+const SequelizeStore = require('connect-session-sequelize')(session.Store);
+const sessionStore = new SequelizeStore({ db });
+app.use(
+  session({
+    secret: process.env.SESSION_SECRET || 'my best friend is Cody',
+    store: sessionStore,
+    resave: false,
+    saveUninitialized: false,
+  })
+);
+app.use(passport.initialize());
+app.use(passport.session());
 
 // Logging Middleware
 const morgan = require('morgan');
@@ -21,15 +52,15 @@ app.use(bodyParser.urlencoded({ extended: true }));
 // Static Middleware
 app.use(express.static(path.join(__dirname, '../public')));
 
+// API Routes
+app.use('/api', require('./api'));
+
 // Route catchall
 app.get('*', function(req, res, next) {
   res.sendFile(path.join(__dirname, '../public/index.html'));
 });
 
 // StartUp functions
-
-// Sync PostGres DB
-const syncDb = () => db.sync();
 
 // Function to set server to listen on PORT
 const startListen = () =>
