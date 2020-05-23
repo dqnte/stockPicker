db76a0f1eb45c45e4bec3509e339a49bcdbb57e0
Dante Tobar <dante.m.tobar@gmail.com>
2020-05-22 18:06:58 -0700

    User and stock relationships added

--- server/db/models/index.js
+++ server/db/models/index.js
@@ -1,11 +1,23 @@
 const User = require('./User');
 const Trade = require('./Trade');
+const PortfolioItem = require('./PortfolioItem');
+const Stock = require('./Stock');
 
 // Relationship Definitions
 
+// User
 User.hasMany(Trade);
+
+// Portfolio Item
+User.hasMany(PortfolioItem);
+Stock.hasMany(PortfolioItem);
+
+// Trade
+Trade.belongsTo(Stock);
 
 module.exports = {
   User,
   Trade,
+  PortfolioItem,
+  Stock,
 };
