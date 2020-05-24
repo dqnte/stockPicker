dc5e9683a2a16e9b1347ecb1ddc8f448b64e4968
Dante Tobar <dante.m.tobar@gmail.com>
2020-05-23 20:24:37 -0700

    Stock prices summed

--- client/components/Portfolio.js
+++ client/components/Portfolio.js
@@ -8,17 +8,23 @@ class Portfoilo extends React.Component {
       return item.stock.latestPrice * item.quantity + total;
     }, 0);
     return (
-      <div id="portfolio">
-        <h3>{user.name}</h3>
-        <h3>Portfoilo: {portfolioSum}</h3>
-        {user.portfolioItems &&
-          user.portfolioItems.map(item => (
-            <h3 key={item.id}>
-              {item.stock.symbol} - {item.quantity} Shares -{' '}
-              {item.stock.latestPrice}
-            </h3>
-          ))}
-      </div>
+      <React.Fragment>
+        <div id="portfolio">
+          <h3>{user.name}</h3>
+          <h3>Portfoilo: {portfolioSum}</h3>
+          {user.portfolioItems &&
+            user.portfolioItems.map(item => (
+              <h3 key={item.id}>
+                {item.stock.symbol} - {item.quantity} Shares -{' '}
+                {Math.floor(item.stock.latestPrice, -2)}
+              </h3>
+            ))}
+        </div>
+        <div id="Purchase">
+          <h3>Cash ${user.balance / 100}</h3>
+          <form></form>
+        </div>
+      </React.Fragment>
     );
   }
 }
