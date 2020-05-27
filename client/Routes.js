db53f2f8f2f53ae591e7e91a444fb230f4e5c920
Dante Tobar <dante.m.tobar@gmail.com>
2020-05-26 22:26:29 -0700

    Default route redesign

--- client/Routes.js
+++ client/Routes.js
@@ -3,25 +3,29 @@ import { Switch, Route } from 'react-router-dom';
 
 import Login from './components/Login';
 import Register from './components/Register';
-import Portfoilo from './components/Portfolio';
+import Home from './components/Home';
 
 const Routes = props => {
   const { user, setUser } = props;
   const isLoggedIn = user && user.id;
+
+  const unAuthedRoutes = (
+    <React.Fragment>
+      <Route key={1} path="/login" render={() => <Login setUser={setUser} />} />
+      <Route
+        key={2}
+        path="/register"
+        render={() => <Register setUser={setUser} />}
+      />
+      <Route key={3} path="/" render={() => <Login setUser={setUser} />} />
+    </React.Fragment>
+  );
+
   return (
     <Switch>
-      {!isLoggedIn && (
-        <Route path="/login" render={() => <Login setUser={setUser} />} />
-      )}
-      {!isLoggedIn && (
-        <Route path="/register" render={() => <Register setUser={setUser} />} />
-      )}
-
+      {!isLoggedIn && unAuthedRoutes}
       {isLoggedIn && (
-        <Route
-          path="/portfolio"
-          render={() => <Portfoilo user={user} setUser={setUser} />}
-        />
+        <Route key={4} render={() => <Home user={user} setUser={setUser} />} />
       )}
     </Switch>
   );
