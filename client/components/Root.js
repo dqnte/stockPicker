4116b85c3a147e9d4b79ee37f2f1251ce59a94ec
Dante Tobar <dante.m.tobar@gmail.com>
2020-05-29 08:34:39 -0700

    Root styles added

--- client/components/Root.js
+++ client/components/Root.js
@@ -1,13 +1,18 @@
 import React from 'react';
+import { withRouter } from 'react-router-dom';
 import axios from 'axios';
 
 import Routes from '../Routes';
+
+import './root.scss';
+
 class Root extends React.Component {
   constructor() {
     super();
     this.state = { user: {} };
 
     this.setUser = this.setUser.bind(this);
+    this.logout = this.logout.bind(this);
   }
 
   async componentDidMount() {
@@ -18,9 +23,22 @@ class Root extends React.Component {
   setUser(user) {
     this.setState({ user });
   }
+
+  logout() {
+    axios.post('/auth/logout').then(() => {
+      this.setState({ user: {} });
+      this.props.history.push('/login');
+    });
+  }
   render() {
-    return <Routes user={this.state.user} setUser={this.setUser} />;
+    return (
+      <Routes
+        user={this.state.user}
+        setUser={this.setUser}
+        logout={this.logout}
+      />
+    );
   }
 }
 
-export default Root;
+export default withRouter(Root);
